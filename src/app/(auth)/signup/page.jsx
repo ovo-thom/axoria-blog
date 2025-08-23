b81617bf6a22b41c96d236b7fcdf31d9c947b1bb
Thomas OvO <thonnard.thomas@gmail.com>
2025-08-23 23:13:00 +0200

    feat: add 53. UI de la page d'inscription

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,7 +1,63 @@
 export default function page() {
   return (
-    <div>
-      <h1 className="mt-20">Sign up</h1>
-    </div>
+    <form className="max-w-md mx-auto mt-36">
+      <label htmlFor="userName" className="f-label">
+        Name or pseudo
+      </label>
+      <input
+        className="f-auth-input"
+        type="text"
+        id="userName"
+        name="userName"
+        placeholder="Name or pseudo"
+        required
+      />
+
+      <label htmlFor="email" className="f-label">
+        E-mail
+      </label>
+      <input
+        className="f-auth-input"
+        type="email"
+        id="email"
+        name="email"
+        placeholder="E-mail"
+        required
+      />
+
+      <label htmlFor="password" className="f-label">
+        Password
+      </label>
+      <input
+        className="f-auth-input"
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Your password"
+        required
+      />
+
+      <label htmlFor="passwordRepeat" className="f-label">
+        Confirm password
+      </label>
+      <input
+        className="f-auth-input mb-14"
+        type="password"
+        id="passwordRepeat"
+        name="passwordRepeat"
+        placeholder="Confirm password"
+        required
+      />
+
+      <button className="w-full bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-3 px-4 my-10  rounded border-none">
+        Submit
+      </button>
+      <a
+        href="/signin"
+        className="mb-5 underline text-blue-600 block text-center"
+      >
+        Already have an account ? Log in
+      </a>
+    </form>
   );
 }
