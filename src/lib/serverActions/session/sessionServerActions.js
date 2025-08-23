81e159fc7cf68a5448c3472b17a5f56960b57e09
Thomas OvO <thonnard.thomas@gmail.com>
2025-08-24 00:17:42 +0200

    feat: add 55. Créer la server action register

---
+++ src/lib/serverActions/session/sessionServerActions.js
@@ -0,0 +1,49 @@
+import { User } from "@/lib/models/user";
+import { connectToDB } from "@/lib/utils/db/connectToDB";
+
+export async function register(formData) {
+  const { userName, email, password, passwordRepeat } =
+    Object.fromEntries(formData);
+
+  if (userName.length < 3) {
+    throw new Error("Username is too short");
+  }
+
+  if (password.length < 6) {
+    throw new Error("Password is too short");
+  }
+
+  if (password !== passwordRepeat) {
+    throw new Error("Passwords do not match");
+  }
+
+  try {
+    connectToDB();
+    const user = await User.findOne({ userName });
+
+    if (user) {
+      throw new Error("Username already exists");
+    }
+
+    const normalizeUserName = slugify(userName, { lower: true, strict: true });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      userName,
+      normalizeUserName,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    console.log("saved to db");
+
+    return { succes: true };
+  } catch (error) {
+    console.log("Error while signin up the user :", err);
+
+    throw new Error(err.message || "An error occured while signin up the user");
+  }
+}
