18cea90471145557da1f87c4cf3c57bc139057c4
Thomas OvO <thonnard.thomas@gmail.com>
2025-08-23 17:37:58 +0200

    feat: add 49. Styliser un article

--- src/app/article/[slug]/page.jsx
+++ src/app/article/[slug]/page.jsx
@@ -1,5 +1,6 @@
 import { getPost } from "@/lib/serverMethods/blog/postMethods";
 import Link from "next/link";
+import "./article-styles.css"
 
 export default async function page({ params }) {
   const { slug } = await params;
