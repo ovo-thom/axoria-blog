2830c87a92a783e05c77abefbc30c8b233926d7f
Thomas OvO <thonnard.thomas@gmail.com>
2025-08-24 18:34:12 +0200

    feat: add 58. Comment fonctionne une authentification par session de base de données ? 59. Création du modèle "session"

---
+++ src/lib/models/session.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const sessionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+  },
+});
+
+export const Session =
+  mongoose.models?.Session || mongoose.model("Session", sessionSchema);
