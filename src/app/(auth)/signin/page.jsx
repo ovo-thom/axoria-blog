5213ab9a79735fc6baf67439e07d817322566728
Thomas OvO <thonnard.thomas@gmail.com>
2025-08-24 18:11:06 +0200

    feat: add 57. Coder l'UI de la page de connexion

--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -1,7 +1,44 @@
+"use client";
+import { useRef } from "react";
+
 export default function page() {
+    const serverInfoRef = useRef();
+    const submitButtonRef = useRef();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div>
-      <h1 className="mt-20">Sign in</h1>
-    </div>
+    <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-36">
+      <label htmlFor="userName" className="f-label">
+        Your pseudo
+      </label>
+      <input
+        type="text"
+        className="f-auth-input"
+        id="userName"
+        required
+        placeholder="Your pseudo"
+      />
+
+      <label htmlFor="password" className="f-label">
+        Your password
+      </label>
+      <input
+        type="password"
+        className="f-auth-input"
+        id="password"
+        required
+        placeholder="Your password"
+      />
+
+      <button
+        ref={submitButtonRef}
+        className="w-full bg-indigo-500 hover:bg-indigo-700 text-white font-bold mt-6 mb-10 py-3 px-4 my-8  rounded border-none"
+      >
+        Submit
+      </button>
+      <p ref={serverInfoRef} className="text-center mb-10"></p>
+    </form>
   );
 }
