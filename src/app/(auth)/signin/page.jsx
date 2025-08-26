26361fcc67477a0fd64b7a05c434bc29151c6598
Thomas OvO <thonnard.thomas@gmail.com>
2025-08-26 22:15:47 +0200

    feat: add 61. Gérer l'envoi du formulaire de connexion

--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -1,12 +1,30 @@
 "use client";
 import { useRef } from "react";
+import { login } from "@/lib/serverActions/session/sessionServerActions";
+import { useRouter } from "next/navigation";
 
 export default function page() {
-    const serverInfoRef = useRef();
-    const submitButtonRef = useRef();
+  const serverInfoRef = useRef();
+  const submitButtonRef = useRef();
+
+  const router = useRouter();
 
   async function handleSubmit(e) {
     e.preventDefault();
+    serverInfoRef.current.textContent = "";
+    submitButtonRef.current.disabled = true;
+
+    try {
+      const result = await login(new FormData(e.target));
+
+      if (result.success) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log("Error during login: error");
+      submitButtonRef.current.disabled = false;
+      serverInfoRef.current.textContent = error.message;
+    }
   }
   return (
     <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-36">
@@ -17,6 +35,7 @@ export default function page() {
         type="text"
         className="f-auth-input"
         id="userName"
+        name="userName"
         required
         placeholder="Your pseudo"
       />
@@ -28,6 +47,7 @@ export default function page() {
         type="password"
         className="f-auth-input"
         id="password"
+        name="password"
         required
         placeholder="Your password"
       />
